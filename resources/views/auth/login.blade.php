e453d6e726a40c271ef0854e183be739608c30b9
Alex Caloggero <a.calog@protonmail.com>
2024-05-29 13:44:03 -0400

    Login page;basic homepage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <h1>To Do Login Form</h1>
 <form action="{{ route('login') }}" method="POST">
-    
+    @csrf
     <label for=""></label>
-    <input type="text" name="" id="" placeholder="email" class="email">
+    <input type="text" name="email" id="" placeholder="email" class="email">
       
     <label for=""></label>
-    <input type="password" name="" id="" placeholder="password" class="pass">
+    <input type="password" name="password" id="" placeholder="password" class="pass">
       
     <button type="submit">login to your account</button>
       
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/welcome', function () {
+    User::updateOrCreate(
+        ['email' => 'caloggero.a@gmail.com', 'name' => 'alex'],
+        ['password' => Hash::make('alex')]
+    );
     return view('welcome');
 });
 
@@ -20,4 +27,18 @@ Route::get('/home', function () {
 
 Route::get('/login', function () {
     return view('auth.login');
+})->name('login');
+
+Route::post('/login', function (Request $request) {
+    $password = $request->input('password');
+    $email    = $request->input('email');
+
+    if (Auth::attempt(['email' => $email, 'password' => $password])) {
+        $request->session()->regenerate();
+        return '200 OK';
+    }
+
+    return back()->withErrors([
+        'email' => 'The provided credentials do not match our records.',
+    ])->onlyInput('email');
 })->name('login');
