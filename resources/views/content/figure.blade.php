34b240acad5936b4dfb6a3590403e8e53ad85d95
Alex Caloggero <a.calog@protonmail.com>
2024-06-25 12:42:23 -0400

    Display thumbnails in gallery

--- resources/views/content/figure.blade.php
+++ resources/views/content/figure.blade.php
@@ -1,6 +1,6 @@
 <figure class="gallery__thumb">
     <a href="{{ route('image.details', ['id' => $id]) }}">
-        <img src="@if(isset($image->loadNow)){{ Storage::disk('s3')->url($image->filename) }}@endif" alt="{{ $description }}" class="gallery__image @if(!isset($image->loadNow)){{ 'lazy-load' }}@endif" data-filename="{{ $filename }}">
+        <img src="@if(isset($image->loadNow)){{ Storage::disk('s3')->url('thumbnails/' . $image->filename) }}@endif" alt="{{ $description }}" class="gallery__image @if(!isset($image->loadNow)){{ 'lazy-load' }}@endif" data-filename="{{ $filename }}">
         <figcaption class="gallery__caption">{{ $description }}</figcaption>
     </a>
 </figure>
